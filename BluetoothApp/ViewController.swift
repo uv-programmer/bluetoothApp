532eaf4ba810652c5a02dc7b98957514807330c2
Ajay-at-196395329624 <ajay@arackal.in>
2022-02-28 05:31:03 +0530

    Initial Commit

---
+++ BluetoothApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothApp
+//
+//  Created by Ajay on 28/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
